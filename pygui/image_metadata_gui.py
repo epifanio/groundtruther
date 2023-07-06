adbb09e994080c87cad9c220cb31cf7465ca822e
epifanio <epiesasha@me.com>
2023-07-06 11:18:12 +0200

    modify datetime class to allon ns resolution

--- pygui/image_metadata_gui.py
+++ pygui/image_metadata_gui.py
@@ -7,6 +7,21 @@ from PyQt5.QtWidgets import *
 
 from groundtruther.pygui.Ui_image_metadata_ui import Ui_imagemetadata
 
+class ExtendedDateTimeEdit(QDateTimeEdit):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setDisplayFormat("yyyy-MM-dd HH:mm:ss.zzz")
+
+    def stepBy(self, steps):
+        current_datetime = self.dateTime()
+
+        # Add or subtract the step value to the current datetime
+        updated_datetime = current_datetime.addSecs(steps)
+
+        # Update the QDateTimeEdit value
+        self.setDateTime(updated_datetime)
+
 
 class ImageMetadata(QWidget, Ui_imagemetadata):
     def __init__(self, parent=None):
