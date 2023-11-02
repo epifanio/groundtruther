a1ee98a581cbfac470fd7b7833e6922973249aa4
Peter Urban <peter.urban@ugent.be>
2023-11-02 11:49:28 +0100

    towards working solver.py: fixed:  - name 'execfile' is not defined  - wrong indentation ('self' not defined'
    not fixed:
     - executing get_pip.py does not do anything (no module named pip)

--- dependencies/solver.py
+++ dependencies/solver.py
@@ -7,65 +7,60 @@ class SolveDependencies():
         'check if pip is installed if not run get_pip and upgrade it'
         self.pip_dir = os.path.dirname(__file__)
 
-    try:
-        import pip
-    except:
-        execfile(os.path.join(self.pip_dir, 'get_pip.py'))
-        import pip
-        # just in case the included version is old
-        pip.main(['install','--upgrade','pip'])
-    try:
-        import pandas
-    except:
-        print('pandas is not installed')
-        pip.main(['install', 'pandas'])
-    try:
-        import pyqtgraph
-    except:
-        print('pyqtgraph is not installed')
-        pip.main(['install', 'pyqtgraph'])
-    # try:
-    #     import pyopengl
-    # except:
-    #     print('pyopengl is not installed')
-    #     pip.main(['install', 'pyopengl'])
-    try:
-        import pydantic
-    except:
-        print('pydantic is not installed')
-        pip.main(['install', 'pydantic'])
-    try:
-        import starlette
-    except:
-        print('starlette is not installed')
-        pip.main(['install', 'starlette'])
-    try:
-        import skimage
-    except:
-        print('scikit-image is not installed')
-        pip.main(['install', 'scikit-image'])
-    try:
-        import plotnine
-    except:
-        print('plotnine is not installed')
-        pip.main(['install', 'plotnine'])
-    try:
-        import pyarrow
-    except:
-        print('pyarrow is not installed')
-        pip.main(['install', 'pyarrow'])
-    try:
-        import numba
-    except:
-        print('numba is not installed')
-        pip.main(['install', 'numba'])
-    try:
-        import simplekml
-    except:
-        print('simplekml is not installed')
-        pip.main(['install', 'simplekml'])
-    try:
-        import geojson
-    except:
-        print('geojson is not installed')
-        pip.main(['install', 'geojson'])
+        try:
+            import pip
+        except:
+            exec(open(os.path.join(self.pip_dir, 'get_pip.py')).read())
+            import pip
+            # just in case the included version is old
+            pip.main(['install','--upgrade','pip'])
+        try:
+            import pandas
+        except:
+            print('pandas is not installed')
+            pip.main(['install', 'pandas'])
+        try:
+            import pyqtgraph
+        except:
+            print('pyqtgraph is not installed')
+            pip.main(['install', 'pyqtgraph'])
+        try:
+            import pyopengl
+        except:
+            print('pyopengl is not installed')
+            pip.main(['install', 'pyopengl'])
+        try:
+            import pydantic
+        except:
+            print('pydantic is not installed')
+            pip.main(['install', 'pydantic'])
+        try:
+            import starlette
+        except:
+            print('starlette is not installed')
+            pip.main(['install', 'starlette'])
+        try:
+            import skimage
+        except:
+            print('scikit-image is not installed')
+            pip.main(['install', 'scikit-image'])
+        try:
+            import plotnine
+        except:
+            print('plotnine is not installed')
+            pip.main(['install', 'plotnine'])
+        try:
+            import pyarrow
+        except:
+            print('pyarrow is not installed')
+            pip.main(['install', 'pyarrow'])
+        try:
+            import numba
+        except:
+            print('numba is not installed')
+            pip.main(['install', 'numba'])
+        try:
+            import simplekml
+        except:
+            print('simplekml is not installed')
+            pip.main(['install', 'simplekml'])
